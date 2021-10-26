40bb0fd856aee4bf647a3103b22c9a430a3c55dd
Simon Cropp <simon.cropp@gmail.com>
2021-10-26 17:09:14 +1100

    Update ReplicantTests.cs

--- Sample/ReplicantTests.cs
+++ Sample/ReplicantTests.cs
@@ -4,25 +4,25 @@
 [UsesVerify]
 public class ReplicantTests
 {
-    static HttpCache httpCache;
+    static HttpCache cache;
 
     static ReplicantTests()
     {
         var directory = Path.Combine(AttributeReader.GetSolutionDirectory(), "ReplicantCache");
-        httpCache = new HttpCache(directory);
+        cache = new HttpCache(directory);
     }
 
     [Fact]
     public async Task Json()
     {
-        var json = await httpCache.StringAsync("https://raw.githubusercontent.com/LearnWebCode/json-example/master/animals-1.json");
+        var json = await cache.StringAsync("https://raw.githubusercontent.com/LearnWebCode/json-example/master/animals-1.json");
         await Verifier.Verify(json);
     }
 
     [Fact]
     public async Task Image()
     {
-        using var response = await httpCache.ResponseAsync("https://upload.wikimedia.org/wikipedia/commons/6/6e/Golde33443.jpg");
+        using var response = await cache.ResponseAsync("https://upload.wikimedia.org/wikipedia/commons/6/6e/Golde33443.jpg");
         await Verifier.Verify(response);
     }
 }
